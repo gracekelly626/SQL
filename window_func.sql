ddcda8b8c4567fe3079ba456dd745cbd9044c900
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-11-07 14:58:28 +0000

    Update window_func.sql

--- window_func.sql
+++ window_func.sql
@@ -1,4 +1,25 @@
-# window function
+# Window Function
+
+## Examples
+note: rank() will skip the ranking numbers, e.g. 1,1,1,4,4,6; while dense_rank() will show 1,1,1,2,2,3,3,3,3,4; row_number() will show 1234
+
+-- rank()
+select *, 
+rank() over(partition by depart_name order by salary desc) as rnk,
+dense_rank over(partition by depart_name order by salary) as DENSE_rnk,
+row_number() over(partition by dept_name order by salary) as rn
+from XX table 
+
+-- lag&lead - fetch a query to display if the salary of an employer is higher, lower or equal to the previous employee
+select *, 
+lag(salary,2,0) over (partition by dept_name order by id) as pre_em_salary
+case when salary > lag(salary,2,0) over (partition by dept_name order by id) then ' higher than previous emplyee'
+     when salary < lag(salary,2,0) over (partition by dept_name order by id) then 'lower'
+     when salary = lag(salary,2,0) over (partition by dept_name order by id) then 'same'
+     end as result
+ from employee
+
+## Facebook Question
 which countries have risen in the rankings based on the number of comments between Dec 2019 vs Jan 2020.
 -- create table for Dec 2019 ranking 
 -- create table for Jan 2020 ranking
@@ -30,11 +51,4 @@ on c.country = d.country
 where c.jan_ranking > d.dec_ranking
 
 
-note: rank() will skip the ranking numbers, e.g. 1,1,1,4,4,6; while dense_rank() will show 1,1,1,2,2,3,3,3,3,4; row_number() will show 1234
 
--- rank()
-select *, 
-rank() over(partition by depart_name order by salary desc) as rnk,
-dense_rank over(partition by depart_name order by salary) as DENSE_rnk,
-row_number() over(partition by dept_name order by salary) as rn
-from XX table 
