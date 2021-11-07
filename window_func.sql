cf054037ebfd72a1a3d1a5f8f086bdda8042b4c2
gracekelly626 <90355504+gracekelly626@users.noreply.github.com>
2021-11-07 00:10:54 +0000

    Update window_func.sql

--- window_func.sql
+++ window_func.sql
@@ -11,7 +11,8 @@ select country, sum(no_comments) as no_comments_dec
 from fb_active_user b
 left join fb_comments a 
 on a.user_id = b.user_id
-where created_at between XX and XX)
+where created_at between '2019-12-01' and '2019-12-31'
+group by country)
 
 with jan_summary as (
 select country, sum(no_comments) as no_comments_jan,
@@ -19,13 +20,14 @@ WINDOW
 from fb_active_user b
 left join fb_comments a 
 on a.user_id = b.user_id
-where created_at between XX and XX)
+where created_at between '2020-01-01' and '2020-01-01'
+group by country)
 
 select * 
 from jan_summary c
 left join dec_summary d
 on c.country = d.country
-where jan_ranking > dec_ranking
+where c.jan_ranking > d.dec_ranking
 
 
 note: rank() will skip the ranking numbers, e.g. 1,1,1,4,4,6; while dense_rank() will show 1,1,1,2,2,3,3,3,3,4
